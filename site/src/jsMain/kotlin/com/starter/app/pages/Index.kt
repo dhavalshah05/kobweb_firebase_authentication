c2057b72ea48768e6f211717137623720410f1d4
Dhaval Shah <dhavalshah@gofynd.com>
2023-08-14 21:52:48 +0530

    Add logout option

--- site/src/jsMain/kotlin/com/starter/app/pages/Index.kt
+++ site/src/jsMain/kotlin/com/starter/app/pages/Index.kt
@@ -2,11 +2,23 @@ package com.starter.app.pages
 
 import androidx.compose.runtime.*
 import com.starter.app.components.AuthenticationWrapper
+import com.starter.app.firebaseApp
 import com.starter.app.theme.AlphaTheme
+import com.starter.app.wrappers.getAuth
+import com.starter.app.wrappers.signOut
+import com.varabyte.kobweb.compose.foundation.layout.Box
+import com.varabyte.kobweb.compose.foundation.layout.Column
+import com.varabyte.kobweb.compose.ui.Alignment
 import com.varabyte.kobweb.compose.ui.Modifier
+import com.varabyte.kobweb.compose.ui.modifiers.fillMaxSize
+import com.varabyte.kobweb.compose.ui.modifiers.fillMaxWidth
+import com.varabyte.kobweb.compose.ui.modifiers.onClick
 import com.varabyte.kobweb.compose.ui.toAttrs
 import com.varabyte.kobweb.core.Page
 import kotlinx.browser.sessionStorage
+import kotlinx.coroutines.await
+import kotlinx.coroutines.launch
+import org.jetbrains.compose.web.dom.Button
 import org.jetbrains.compose.web.dom.H1
 import org.jetbrains.compose.web.dom.Text
 import org.w3c.dom.get
@@ -18,13 +30,43 @@ fun HomePage() {
         sessionStorage["user_id"]
     }
 
+    val scope = rememberCoroutineScope()
+    val auth = remember {
+        getAuth(firebaseApp)
+    }
+
     AuthenticationWrapper {
-        H1(
-            attrs = Modifier
-                .then(AlphaTheme.typography.latoBold)
-                .toAttrs()
+        Box(
+            modifier = Modifier
+                .fillMaxSize(),
+            contentAlignment = Alignment.Center,
         ) {
-            Text("Welcome $userId")
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                horizontalAlignment = Alignment.CenterHorizontally,
+            ) {
+                H1(
+                    attrs = Modifier
+                        .then(AlphaTheme.typography.latoBold)
+                        .toAttrs()
+                ) {
+                    Text("Welcome $userId")
+                }
+
+                Button(
+                    attrs = Modifier
+                        .onClick {
+                            scope.launch {
+                                signOut(auth = auth).await()
+                            }
+                        }
+                        .toAttrs()
+                ){
+                    Text("Logout")
+                }
+            }
         }
+
     }
 }
--- site/src/jsMain/kotlin/com/starter/app/wrappers/firebase_auth.kt
+++ site/src/jsMain/kotlin/com/starter/app/wrappers/firebase_auth.kt
@@ -9,3 +9,4 @@ external fun getAuth(config: dynamic): dynamic
 external fun signInWithEmailAndPassword(auth: dynamic, email: String, password: String): Promise<dynamic>
 external fun createUserWithEmailAndPassword(auth: dynamic, email: String, password: String): Promise<dynamic>
 external fun onAuthStateChanged(auth: dynamic, callback: (user: dynamic) -> Unit)
+external fun signOut(auth: dynamic): Promise<dynamic>
